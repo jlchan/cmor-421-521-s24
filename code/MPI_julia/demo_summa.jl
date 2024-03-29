2849750e6d946d837e53029229fc8851f02217e7
Jesse Chan <jesse.lee.chan@gmail.com>
2024-03-28 23:15:05 -0500

    minor

--- code/MPI_julia/demo_summa.jl
+++ code/MPI_julia/demo_summa.jl
@@ -13,14 +13,13 @@ col_color = rank % p
 row_comm = MPI.Comm_split(MPI.COMM_WORLD, row_color, rank)
 col_comm = MPI.Comm_split(MPI.COMM_WORLD, col_color, rank)
 
-x = rank #(row_color, col_color)
-
 original_ranks = MPI.Gather(rank, MPI.COMM_WORLD; root=0)
 if rank==0
     println("\n Ranks")
     show(stdout, "text/plain", Matrix(reshape(original_ranks, p, p))) 
 end
 
+x = rank 
 for k = 0:p-1 # remember MPI is 0-indexed
     row_bcast = MPI.bcast(x, row_comm; root=k)
     col_bcast = MPI.bcast(x, col_comm; root=k)
